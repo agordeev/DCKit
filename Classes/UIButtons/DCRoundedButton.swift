6f502cfd149fbb1220e269eecf2582f32d1db27c
Andrey Gordeev <andreygordeev@Andreys-MacBook-Pro.local>
2015-05-10 11:36:33 +0300

    Added DCRoundedButton.

---
+++ Classes/UIButtons/DCRoundedButton.swift
@@ -0,0 +1,65 @@
+//
+//  DCRoundedButton.swift
+//  DCKitSample
+//
+//  Created by Andrey Gordeev on 5/10/15.
+//  Copyright (c) 2015 Andrey Gordeev. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class DCRoundedButton: DCBaseButton {
+    
+    override public var enabled: Bool {
+        didSet {
+            updateColor()
+        }
+    }
+    
+    @IBInspectable var normalBorderColor: UIColor = UIColor.lightGrayColor() {
+        didSet {
+            updateColor()
+        }
+    }
+    
+    @IBInspectable var disabledBorderColor: UIColor = UIColor.lightGrayColor() {
+        didSet {
+            updateColor()
+        }
+    }
+    
+    @IBInspectable var selectedBorderColor: UIColor = UIColor(red: 37.0/255.0, green: 147.0/255.0, blue: 1.0/255.0, alpha: 1.0) {
+        didSet {
+            updateColor()
+        }
+    }
+    
+    // MARK: - Build control
+    
+    override public func customInit() {
+        super.customInit()
+        
+        addBorder()
+    }
+    
+    public func addBorder() {
+        layer.borderColor = normalBorderColor.CGColor
+        layer.borderWidth = 1.0 / UIScreen.mainScreen().scale
+        layer.cornerRadius = frame.size.height*0.5
+        clipsToBounds = true
+        
+        // http://stackoverflow.com/questions/4735623/uilabel-layer-cornerradius-negatively-impacting-performance
+        layer.masksToBounds = false
+        layer.rasterizationScale = UIScreen.mainScreen().scale
+        layer.shouldRasterize = true
+    }
+    
+    // MARK: - Misc
+    
+    override public func updateColor() {
+        super.updateColor()
+        layer.borderColor = enabled ? (selected ? selectedBorderColor.CGColor : normalBorderColor.CGColor) : disabledBorderColor.CGColor
+    }
+    
+}

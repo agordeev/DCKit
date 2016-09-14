3e9b415e37ba9981753fd98bb3d0cd07e421acd4
Andrey Gordeev <andrew8712@gmail.com>
2016-09-14 11:51:27 +0300

    Created a separate module for DCKit.

---
+++ DCKit/DCKit.h
@@ -0,0 +1,19 @@
+//
+//  DCKit.h
+//  DCKit
+//
+//  Created by Andrey Gordeev on 9/14/16.
+//  Copyright © 2016 Andrey Gordeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DCKit.
+FOUNDATION_EXPORT double DCKitVersionNumber;
+
+//! Project version string for DCKit.
+FOUNDATION_EXPORT const unsigned char DCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCKit/PublicHeader.h>
+
+
